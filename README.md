4981a83af8ebcda6aaa97ef772540ee00a6ad452
Emogy-reunion <mv7786986@gmail.com>
2024-05-17 03:06:19 -0700

    first commit

--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 
 ## App initialization
 * I initialized the flask app by import Flask from flask
+
+## Database Initialization
+* To initialize my database, I imported SQLAlchemy from flask_alchemy
+* SQLAlchemy will be used to map the classes to tables
+* I declared three models User, Property, Image
+* User stores user information such as users  first name, last name, email, password, properties associated with the user
+* properties is a relationship attribute that establishes a one to many relationship with the Property table
+* This means one user can be associated with multiple images
+* Property class stores property details such as its location, price, bedrooms, images, features etc
+* images attribute stores property images
+* It establishes a one to many relationship with the Image class meaning one property can have multiple images
+* Image class stores the file names of images associated with a property
--- app.py
+++ app.py
@@ -1,8 +1,51 @@
 from flask import Flask
+from flask_alchemy import SQLAlchemy
 
 
 #Initiliaze flask app
 app = Flask(__name__)
+
+#configure sqlalchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = ""
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    """Class to store user information"""
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(50), unique=True, nullable=False)
+    phone_number = db.Column(db.Integer, unique=True, nullable=False)
+    agency = db.Column(db.String, unique=True, nullable=False)
+    properties = relationship('Property', backref='user', lazy=True)
+
+
+class Property(db.Model):
+    """Maps table to store property information"""
+    __tablename__ = 'properties'
+
+    id = db.Column(db.Integer, primary_key=True)
+    location = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    bedrooms = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    features = db.Column(db.String(250), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    images = relationship('Image', backref='property', lazy=True)
+
+
+class Image(db.Model):
+    """stores property filenames"""
+    __tablename__ = "images"
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(100), nullable=False)
+    property_id = db.Column(db.Integer, db.ForeignKey('properties.id'), nullable=False)
+
+    
+db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True)
