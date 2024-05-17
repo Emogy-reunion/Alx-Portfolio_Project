1eadb375b4ba4b3d3e093f1e21c51f29e2226b7b
Emogy-reunion <mv7786986@gmail.com>
2024-05-17 07:14:32 -0700

    handled authentication

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect, request
+from sqlalchemy.orm import relationship
 from flask_alchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user
 
 
 #Initiliaze flask app
 app = Flask(__name__)
 
-#configure sqlalchemy
+#configure sqlalchemy, flask login
+app.config['SECRET_KEY'] = 'MY_KEY'
 app.config['SQLALCHEMY_DATABASE_URI'] = ""
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     """Class to store user information"""
     __tablename__ = 'users'
 
@@ -17,9 +23,9 @@ class User(db.Model):
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(50), unique=True, nullable=False)
-    phone_number = db.Column(db.Integer, unique=True, nullable=False)
-    agency = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String(50), unique=False, nullable=False)
+    phone_number = db.Column(db.Integer, unique=False, nullable=False)
+    agency = db.Column(db.String, unique=False, nullable=False)
     properties = relationship('Property', backref='user', lazy=True)
 
 
@@ -70,6 +76,44 @@ def rent():
 def contact():
     """renders the contact page of the main site"""
     return render_template('contact.html')
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/login', methods=['GET', 'POST'], endpoint='login')
+def login():
+    """handles login request"""
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first()
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('dashboard'))
+        else:
+            flash('Invalid username or password. Please try again.')
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'], endpoint='register')
+def register():
+    """handles creating account request"""
+    if request.method == 'POST':
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        email = request.form['email']
+        phone_number = request.form['phone_number']
+        agency = request.form['agency']
+        password = request.form['password']
+        if User.query.filter_by(email=email).first():
+            flash('There is an account associated with this email.')
+        else:
+            hashed_password = generate_password_hash(password)
+            new_user = User(first_name=first_name, last_name=last_name, email=email, phone_number=phone_number, agency=agency, password=hashed_password)
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect(url_for('login'))
+    return render_template('create_account.html')
 
 
 if __name__ == '__main__':
