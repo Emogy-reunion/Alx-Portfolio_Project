3000786fd3ec464fcea16adbfef0978ebb958fa6
Emogy-reunion <mv7786986@gmail.com>
2024-05-28 07:09:55 -0700

    first commit

---
+++ static/js/rent.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const items = document.querySelectorAll('.item');
+
+    items.forEach(item => {
+        item.addEventListener('click', function() {
+            const propertyId = this.dataset.property_id;
+
+            fetch(`/property_details/${propertyId}`)
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    // Handle the response if necessary
+                })
+                .catch(error => {
+                    console.error('There was a problem with the fetch operation:', error);
+                });
+        });
+    });
+});
---
+++ static/js/uploads.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const items = document.querySelectorAll('.item');
+
+    items.forEach(item => {
+        item.addEventListener('click', function() {
+            const propertyId = this.dataset.property_id;
+
+            fetch(`/details/${propertyId}`)
+                .then(response => {
+                    if (!response.ok) {                                                                         throw new Error('Network response was not ok');
+                    }
+                })
+                .catch(error => {
+                    console.error('There was a problem with the fetch operation:', error);
+                });
+        });
+    });
+});
